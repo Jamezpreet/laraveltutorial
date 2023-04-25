008057940613ae00cfebe429124469a8f1eef961
jamezpreet <jamespreet112000@gmail.com>
2023-04-25 22:30:51 +0530

    form validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -8,6 +8,11 @@ class FormController extends Controller
 {
     public function getData(Request $request)
     {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
         return $request->all();
     }
 }
--- resources/views/FormData/form.blade.php
+++ resources/views/FormData/form.blade.php
@@ -4,7 +4,11 @@
     @csrf
     <input type="text" name="username">
     <br><br>
+    <span style="color: red"> @error('username') {{$message}} @enderror</span>
+    <br><br>
     <input type="password" name="password">
+    <br><br>
+    <span style="color: red"> @error('password') {{$message}} @enderror</span>
     <br><br>
     <button> Submit </button>
 </form>
